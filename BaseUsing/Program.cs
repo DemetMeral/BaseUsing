5f8360eedb21a45c92e09adf006708e7623b5176
DemetMeral <106647124+DemetMeral@users.noreply.github.com>
2022-09-10 10:21:08 +0300

    Add project files.

---
+++ BaseUsing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BaseUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class A:B
+    {
+    
+    }
+
+    class B
+    {
+
+
+    }
+}
